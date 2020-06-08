1b1eebdf0c62e14ce9c90967fc94c81ec9b4f002
angeluidev <angeluidev@gmail.com>
2020-06-08 23:23:12 +0800

    Updates

---
+++ precache-manifest.a6064f222b109dddf8bcf7b15f843f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f86bee0b0e44ec77b17d8eff85b2e6",
+    "url": "/my-to-do-list/index.html"
+  },
+  {
+    "revision": "ac553c2f254c2f3b3a3c",
+    "url": "/my-to-do-list/static/css/main.361022a0.chunk.css"
+  },
+  {
+    "revision": "47bb9500157f80845c8c",
+    "url": "/my-to-do-list/static/js/2.37e4963b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-to-do-list/static/js/2.37e4963b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac553c2f254c2f3b3a3c",
+    "url": "/my-to-do-list/static/js/main.6bf6a2cd.chunk.js"
+  },
+  {
+    "revision": "f8dd008090fdd51d41ad",
+    "url": "/my-to-do-list/static/js/runtime-main.99aa7feb.js"
+  }
+]);
